9e88c83a80049a5f169b0975e67274e5cacdec75
ycachy <1021487797@qq.com>
2022-07-12 23:06:55 +0800

    Update and rename tensor_ggnn_gcn.py to ggtn.py

--- src/process/models/ggtn.py
+++ src/process/models/ggtn.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from typing import List, Tuple
 
 
-class Tensor_GGNN_GCN(MessagePassing):
+class GGTN(MessagePassing):
     def __init__(self,
                  num_edge_types,
                  #in_features,
@@ -25,7 +25,7 @@ class Tensor_GGNN_GCN(MessagePassing):
                  aggr="mean",
                  device="cpu",
                  output_model="learning"):
-        super(Tensor_GGNN_GCN, self).__init__(aggr=aggr)
+        super(GGTN, self).__init__(aggr=aggr)
         # params set
         self.num_edge_types = num_edge_types
         self.device = device
@@ -33,7 +33,7 @@ class Tensor_GGNN_GCN(MessagePassing):
         self.dropout = dropout
         self.max_node_per_graph=max_node_per_graph
         #self.max_variable_candidates = max_variable_candidates
-        # 先对值进行embedding
+      
         #self.value_embeddingLayer = EmbeddingLayer(embedding_num_classes,
                                                    #in_features,
                                                    #embedding_features,
@@ -50,7 +50,7 @@ class Tensor_GGNN_GCN(MessagePassing):
         
         self.conv1_list = nn.ModuleList([GCNConv(out_features, out_features, add_self_loops=add_self_loops) for _ in range(self.num_edge_types)] )
         self.interConv = InterConv(out_features, out_features, num_edge_types=self.num_edge_types)
-        self.thirdNorm = torch.nn.InstanceNorm2d(out_features)  # 这个就是自己要找的
+        self.thirdNorm = torch.nn.InstanceNorm2d(out_features)  
 
         self.lin = nn.Linear(out_features*2, out_features)
         self.conv1 = GCNConv(out_features, out_features, add_self_loops=add_self_loops)
@@ -70,7 +70,7 @@ class Tensor_GGNN_GCN(MessagePassing):
             for i in range(len(edge_list)):
                 edge = edge_list[i]
                 if edge.shape[0] != 0 :
-                    # 该种类型的边存在边
+                  
                     out_list.append(self.MessagePassingNN[i](cur_node_states, edge))
             cur_node_states = sum(out_list)
             new_node_states = self.gru_cell(cur_node_states, last_node_states)  # input:states, hidden
@@ -78,7 +78,7 @@ class Tensor_GGNN_GCN(MessagePassing):
 
         ggnn_out = last_node_states # shape: V, D
 
-        # tensor GCN new:
+        
         assert self.num_edge_types == 4   #4
         cur_x = torch.cat([ggnn_out,ggnn_out,ggnn_out,ggnn_out], dim=0)  # 4V, D
         loop_edge_list = self.matrix_loop(edge_list) # 4V, 4V
@@ -99,7 +99,7 @@ class Tensor_GGNN_GCN(MessagePassing):
         return edge_new
 
     def matrix_loop(self, edge_list):
-        # 4个邻接矩阵并列。
+     
         assert len(edge_list) == 4
         A1, A2, A3, A4 = edge_list
         n = self.max_node_per_graph
